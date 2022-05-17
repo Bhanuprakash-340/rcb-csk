0545a8c96ffaa4819ed79d61c7c355ab73da9f4e
Bhanuprakash-340 <bhanu150940@gmail.com>
2022-05-17 07:11:56 +0000

    first-comit

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div className="bg-container">
+    <div class="block-container">
+      <h1 className="heading">Super Over League</h1>
+      <div className="image-container">
+        <img
+          className="rcb-image"
+          src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        />
+        <img
+          className="csk-image"
+          src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        />
+      </div>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
